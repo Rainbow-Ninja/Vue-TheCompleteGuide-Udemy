e7d7c96b60ba599e6791b3f51f47c2f5e28c65b3
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2022-07-26 11:57:17 +1000

    events and modifiers

--- 2/events/app.js
+++ 2/events/app.js
@@ -1,9 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      favColour: '',
+      fullName: '',
+      confirmedName: '',
     };
   },
+  methods: {
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    favouriteColour(event) {
+      this.favColour = event.target.value;
+    },
+    setFullName(event, lastName) {
+      this.fullName = event.target.value + ' ' + lastName;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      alert('Submitted!');
+    },
+    confirmName() {
+      this.confirmedName = this.fullName;
+    }
+  }
 });
 
 app.mount('#events');
