fd830f8c0efba4e8b94e12274d0e631126d4b793
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2022-07-28 15:51:00 +1000

    next class

---
+++ 5/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      goals: [],
+      enteredGoalValue: '' 
+  };
+  },
+  methods: {
+    addGoal() {
+      if( this.enteredGoalValue) {
+        this.goals.push(this.enteredGoalValue);
+        this.enteredGoalValue = '';
+      }
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
+    }
+  }
+});
+
+app.mount('#user-goals');
